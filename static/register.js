3d01f0b70ed570651fc0a1567275668af44fb15d
Ephraim Lefkowitz <elefkowitz@cardknox.com>
2024-03-12 11:45:52 -0400

    Validate email

--- static/register.js
+++ static/register.js
@@ -1,14 +1,29 @@
 const registerbtn = document.getElementById("registerbtn");
 const message = document.getElementById("message");
+const userEmail = document.getElementById("username");
+
+function validateEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 registerbtn.addEventListener("click", function () {
-    var formData = {};
-    var fields = ["username", "password"];
-    fields.forEach(function (field) {
-        formData[field] = document.getElementById(field).value;
-    });
-    formData['tranzType'] = "N";
-    var formDataJSON = JSON.stringify(formData);
-    sendtoserver(formDataJSON)
+    const currentemail = userEmail.value;
+    if (validateEmail(currentemail)) {
+        
+        var formData = {};
+        var fields = ["username", "password"];
+        fields.forEach(function (field) {
+            formData[field] = document.getElementById(field).value;
+        });
+        formData['tranzType'] = "N";
+        var formDataJSON = JSON.stringify(formData);
+        sendtoserver(formDataJSON)
+
+    } else {
+        message.innerHTML = "Invalid email address";
+    }
+
+
 });
 
 function sendtoserver(serverdata) {
@@ -22,12 +37,12 @@ function sendtoserver(serverdata) {
     })
         .then(response => response.json())
         .then(data => {
-            
+
             console.log(data.status);
             if (data.status == 'fail') {
                 console.log(data);
                 message.innerHTML = data.message;
-            }if(data.status == 'success'){
+            } if (data.status == 'success') {
                 console.log(data.redirect);
                 window.location.href = data.redirect;
             }
